2b504dd04700bfd5ea7db7acdfec0fa6a5514e55
Abinash Anand <74396889+Abinash-Anand@users.noreply.github.com>
2021-10-18 21:58:21 +0530

    JS fundamentals part 2

---
+++ script.js
@@ -0,0 +1,10 @@
+/* Activating strict mode
+1. strict mode helps to show the errors in the code which is usually silently hidden in javascript
+2. It also helps to restict the coder to some extent thus helps to reduce bugs in the code 
+*/
+//syntax of "strict mode"
+"use strict";
+//Functions in js
+function fruitJuice(apple, orange) {
+  console.log(apple, orange);
+}
